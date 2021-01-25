0d711237552cb37cf91a704bea7214d1528f45f8
Weronika Jaworek <s18630@pjwstk.edu.pl>
2021-01-25 20:37:23 +0100

    Początki update

--- Cwiczenie5/Cwiczenie5/Controllers/StudentsController.cs
+++ Cwiczenie5/Cwiczenie5/Controllers/StudentsController.cs
@@ -2,19 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Cwiczenie5.DTOs.Requests;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Cwiczenie5.Controllers
 {
-    [Route("api/[controller]")]
+   // [Route("api/[controller]")]
     [ApiController]
     public class StudentsController : ControllerBase
     {
        
 
-
+        //1.zwraca liste studentów
         [HttpGet]
+        [Route("api/students/lista")]
         public IActionResult GetStudents()
         {
             var db = new _2019SBDContext();
@@ -22,5 +24,45 @@ namespace Cwiczenie5.Controllers
 
             return Ok(res);
         }
+
+
+
+        //2. modyfikacja danych studenta
+
+        [HttpPost]
+        [Route("api/students/modify-Studet")] //zmień na student
+        public IActionResult ModifyStudent(ModifyStudentRequest request)
+        {
+            var db = new _2019SBDContext();
+            //  var res = db.Student.ToList();
+
+
+            var student = new Student
+            {
+                IndexNumber = request.IndexNumber,
+                FirstName =request.FirstName,
+                LastName = request.LastName,
+                BirthDate=request.BirthDate
+            };
+            db.Attach(student); 
+            //sprawdź czy student istnieje ?
+           db.Entry(student).Property("FirstName").IsModified = true;
+           db.Entry(student).Property("LastName").IsModified = true;
+            db.Entry(student).Property("BirthDate").IsModified = true; // jak sie nie wpisze dodaje się automatyczne jeśli są wymogi null
+            //wtedy wtsrępuje bład 
+            //spróbuj drugą wersję
+
+
+            db.SaveChanges();
+
+
+
+
+            return Ok(request);
+        }
+
+
+
+        //3. usunięcie studenta
     }
 }
---
+++ Cwiczenie5/Cwiczenie5/DTOs/Requests/ModifyStudentRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cwiczenie5.DTOs.Requests
+{
+    public class ModifyStudentRequest
+    {
+        [Required]
+        [RegularExpression("^s[0-9]+$")]
+        public string IndexNumber { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+
+ //       public int IdEnrollment { get; set; }
+ //       public string Password { get; set; }
+
+
+    }
+}
