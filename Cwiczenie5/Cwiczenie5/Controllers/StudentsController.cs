03191dc6119c97da0924acf08ec3e3a305b295e3
Weronika Jaworek <s18630@pjwstk.edu.pl>
2021-01-25 23:37:42 +0100

    metoda delete nei działa

--- Cwiczenie5/Cwiczenie5/Controllers/StudentsController.cs
+++ Cwiczenie5/Cwiczenie5/Controllers/StudentsController.cs
@@ -5,14 +5,15 @@ using System.Threading.Tasks;
 using Cwiczenie5.DTOs.Requests;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Cwiczenie5.Controllers
 {
-   // [Route("api/[controller]")]
+    // [Route("api/[controller]")]
     [ApiController]
     public class StudentsController : ControllerBase
     {
-       
+
 
         //1.zwraca liste studentów
         [HttpGet]
@@ -30,7 +31,7 @@ namespace Cwiczenie5.Controllers
         //2. modyfikacja danych studenta
 
         [HttpPost]
-        [Route("api/students/modify-Studet")] //zmień na student
+        [Route("api/students/modifyStudent")] //zmień na student
         public IActionResult ModifyStudent(ModifyStudentRequest request)
         {
             var db = new _2019SBDContext();
@@ -40,18 +41,19 @@ namespace Cwiczenie5.Controllers
             var student = new Student
             {
                 IndexNumber = request.IndexNumber,
-                FirstName =request.FirstName,
+                FirstName = request.FirstName,
                 LastName = request.LastName,
-                BirthDate=request.BirthDate
+                BirthDate = request.BirthDate
             };
-            db.Attach(student); 
+            db.Attach(student);
             //sprawdź czy student istnieje ?
-           db.Entry(student).Property("FirstName").IsModified = true;
-           db.Entry(student).Property("LastName").IsModified = true;
+            db.Entry(student).Property("FirstName").IsModified = true;
+            db.Entry(student).Property("LastName").IsModified = true;
             db.Entry(student).Property("BirthDate").IsModified = true; // jak sie nie wpisze dodaje się automatyczne jeśli są wymogi null
             //wtedy wtsrępuje bład 
             //spróbuj drugą wersję
-
+            //no i nei sprawdza czy jest student
+            //i trzeba podać wszytskie
 
             db.SaveChanges();
 
@@ -64,5 +66,24 @@ namespace Cwiczenie5.Controllers
 
 
         //3. usunięcie studenta
+        [HttpPost]
+        [Route("api/students/deleteStudent")] //zmień na student
+        public IActionResult DeleteStudent(string indexNumber)
+        {
+
+            var db = new _2019SBDContext();
+            var student = new Student
+            {
+                IndexNumber = indexNumber
+            };
+
+            db.Attach(student);
+           db.Remove(student);
+         
+
+            db.SaveChanges();
+            return Ok(student);
+        }
     }
+
 }
